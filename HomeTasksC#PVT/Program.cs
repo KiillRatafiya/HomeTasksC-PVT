5df718bf62de2853ce73f6d4ac545e4f482def05
KirillRatafiya <ratafiya@gmail.com>
2024-04-17 22:05:54 +0300

    Add project files.

---
+++ HomeTasksC#PVT/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTasksC_PVT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
